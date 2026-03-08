fb8827dac86b045396359edb7a4fb18a1d02bc7e
Tropical <42101043+tropicaaal@users.noreply.github.com>
2026-03-08 03:06:25 -0500

    fix: handle existing Common folder in assets copy script

--- scripts/copy_assets_to_client.py
+++ scripts/copy_assets_to_client.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
 
-from shutil import copytree, copy
-from sys import argv
+import os
+import shutil
+import sys
 from pathlib import Path
 
-project_source_root = Path(argv[1])
-build_root_dir = Path(argv[2])
-client_build_dir = Path(argv[3])
-media_archive = Path(argv[4])
-output_stamp = Path(argv[5])
+project_source_root = Path(sys.argv[1])
+build_root_dir = Path(sys.argv[2])
+client_build_dir = Path(sys.argv[3])
+media_archive = Path(sys.argv[4])
+output_stamp = Path(sys.argv[5])
 
 # At runtime, the client expects a `Common/` folder from the Minecraft.Assets source and a compiled
 # media archive (LinuxMedia.arc) inside of said folder at its current working directory to launch.
 #
 # `meson install` also handles this, but installs it to system folders, which can be annoying for
 # testing. Since we want a way to run it straight from `/build` when debugging, we do this instead.
+dest_common = Path(client_build_dir / "Common")
+
+# clear out any old assets
+if dest_common.exists():
+    shutil.rmtree(dest_common)
 
 # copy `Minecraft.Assets/Common` into the build directory for the client.
-copytree(
+shutil.copytree(
     project_source_root / "Minecraft.Assets" / "Common",
-    client_build_dir / "Common"
+    dest_common,
 )
 
 # copy the media archive to `Common/Media` inside the folder we just copied.
-copy(media_archive, client_build_dir / "Common" / "Media")
+shutil.copy(media_archive, client_build_dir / "Common" / "Media")
 
 # modify the stamp so this only happens when client or media_archive targets are changed
-Path(output_stamp).touch()
+output_stamp.touch()
